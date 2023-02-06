20d658de06c3a6fc2bbf6630c383880c70c8fcd2
Dmitry Chernyshev <chernyshevdv@gmail.com>
2023-02-06 08:35:29 +0100

    Troubleshooting omniauth

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,25 @@
+class SessionsController < ApplicationController
+  skip_before_action :set_current_user
+  def new
+    redirect_to '/auth/twitter'
+  end
+
+  def create
+    auth = request.env["omniauth.auth"]
+    user = Moviegoer.where(:provider => auth['provider'],
+                           :uid => auth['uid'].to_s).first || Moviegoer.create_with_omniauth(auth)
+    session[:user_id] = user.id
+    redirect_to movies_path
+  end
+
+  def destroy
+    session.delete(:user_id)
+    flash[:notice] = 'Logged out successfully'
+    redirect_to movies_path
+  end
+
+  def failure
+    flash[:notice] = "Invalid credentials!"
+    redirect_to movies_path
+  end
+end
