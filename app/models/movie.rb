eccffa5556c8e998ed6825935ba6b71e380d3e73
Dmitry Chernyshev <chernyshevdv@gmail.com>
2023-02-06 23:14:17 +0100

    Through-associations added

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,8 @@
 class Movie < ApplicationRecord
-  has_many :reviews
+  has_many :reviews, :dependent => :destroy
+  has_many :moviegoers, :through => :reviews
+
+  def to_s
+    "#{title} (#{release_date}): #{rating}"
+  end
 end
--- app/models/moviegoer.rb
+++ app/models/moviegoer.rb
@@ -1,5 +1,6 @@
 class Moviegoer < ApplicationRecord
   has_many :reviews
+  has_many :movies, :through => :reviews
 
   def self.create_with_omniauth(auth)
     Moviegoer.create!(
